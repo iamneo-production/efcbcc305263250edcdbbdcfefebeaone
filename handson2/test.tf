218f2bdc029c918689e12f67ce62aff7a29513b5
Iamneo <techadmin@iamneo.ai>
2023-09-20 09:07:41 +0000

    Backup Commit

--- handson2/test.tf
+++ handson2/test.tf
@@ -1,9 +1,12 @@
 provider "aws" {
-    region =""
-    access_key=""
-    secert_key=""
+    region ="ap-northeast-1"
+    access_key="AKIAYOZVCR6PW4Q2MP6G"
+    secert_key="VJIuyOTeriucRJxvsIjmc8jetg+SV1jGtVSxVMOS"
 }
 resource "aws_instance" "server" {
-    ami=""
-    instance_type=""
+    ami="ami-0d52744d6551d851e"
+    instance_type="t2.micro"
+}
+output "Public_ip" {
+    value=aws_instance.server.Public_ip
 }
